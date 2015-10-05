c5adb7c20ea8ba7a27c6fa89833a33e436e519d8
Ian Campbell <ianwcampbell.contact@gmail.com>
2015-10-05 15:07:47 -0400

    added characters and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#stage population
+Stage.create(name: "Final Destination", type: "Neutral")
+Stage.create(name: "Fountain of Dreams", type: "Neutral")
+Stage.create(name: "Battlefield", type: "Neutral")
+Stage.create(name: "Dream Land (N64)", type: "Neutral")
+Stage.create(name: "Yoshi's Story", type: "Neutral")
+
+Stage.create(name: "Pokémon Stadium", type: "Counterpick")
+
+Stage.create(name: "Big Blue", type: "Gentleman")
+Stage.create(name: "Brinstar", type: "Gentleman")
+Stage.create(name: "Brinstar Depths", type: "Gentleman")
+Stage.create(name: "Corneria", type: "Gentleman")
+Stage.create(name: "Flat Zone", type: "Gentleman")
+Stage.create(name: "Fourside", type: "Gentleman")
+Stage.create(name: "Great Bay", type: "Gentleman")
+Stage.create(name: "Green Greens", type: "Gentleman")
+Stage.create(name: "Icicle Mountain", type: "Gentleman")
+Stage.create(name: "Jungle Japes", type: "Gentleman")
+Stage.create(name: "Kongo Jungle", type: "Gentleman")
+Stage.create(name: "Kongo Jungle (N64)", type: "Gentleman")
+Stage.create(name: "Mushroom Kingdom", type: "Gentleman")
+Stage.create(name: "Mushroom Kingdom II", type: "Gentleman")
+Stage.create(name: "Mute City", type: "Gentleman")
+Stage.create(name: "Onett", type: "Gentleman")
+Stage.create(name: "Poké Floats", type: "Gentleman")
+Stage.create(name: "Princess Peach's Castle", type: "Gentleman")
+Stage.create(name: "Rainbow Cruise", type: "Gentleman")
+Stage.create(name: "Temple", type: "Gentleman")
+Stage.create(name: "Vemon", type: "Gentleman")
+Stage.create(name: "Yoshi's Island (pipes)", type: "Gentleman")
+Stage.create(name: "Yoshi's Island (N64)", type: "Gentleman")
