e277fff96e4f6e1dc78e25505a23407bac7911fc
Ian Campbell <ianwcampbell.contact@gmail.com>
2015-10-06 15:24:25 -0400

    working form, broken test

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,13 @@
 class Game < ActiveRecord::Base
+  CHARACTERS = [
+    "Dr. Mario", "Mario", "Luigi", "Bowser", "Peach", "Yoshi",
+    "Donkey Kong", "Captain Falcon", "Ganondorf", "Falco", "Fox",
+    "Ness", "Ice Climbers", "Kirby", "Samus", "Zelda", "Link",
+    "Young Link", "Pichu", "Pikachu", "Jigglypuff", "Mewtwo",
+    "Mr. Game & Watch", "Marth", "Roy"
+  ]
+  STOCK = [4,3,2,1,0]
+
   belongs_to :stage
   validates :player_1_id, presence: true
   validates :player_2_id, presence: true
@@ -7,4 +16,21 @@ class Game < ActiveRecord::Base
   validates :player_1_stock, presence: true
   validates :player_2_stock, presence: true
   validates :stage_id, presence: true
+
+  def winner
+    if player_1_stock < player_2_stock
+      User.find(player_2_id)
+    elsif player_1_stock > player_2_stock
+      User.find(player_1_id)
+    end
+  end
+
+  def loser
+    if player_1_stock < player_2_stock
+      User.find(player_1_id)
+    elsif player_1_stock > player_2_stock
+      User.find(player_2_id)
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  CHARACTERS = [
-    "Dr. Mario", "Mario", "Luigi", "Bowser", "Peach", "Yoshi",
-    "Donkey Kong", "Captain Falcon", "Ganondorf", "Falco", "Fox",
-    "Ness", "Ice Climbers", "Kirby", "Samus", "Zelda", "Link",
-    "Young Link", "Pichu", "Pikachu", "Jigglypuff", "Mewtwo",
-    "Mr. Game & Watch", "Marth", "Roy"
-  ]
-
   validates :email, presence: true
   validates :username, presence: true, length: { maximum: 20 }
+
+  scope :not_me, -> { where.not(id: current_user.id) }
 
   def games
     Game.find(:conditions => ["player_1_id = ? OR player_2_id = ?", id, id])
