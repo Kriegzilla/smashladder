34734a3d8ca3826e57bd245672b71e7d9e1fbfcc
Ian Campbell <ianwcampbell.contact@gmail.com>
2015-10-07 15:59:16 -0400

    added show page, need to redo characters

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'users#index'
   devise_for :users
   resources :users, only: [:index, :show, :destroy]
-  resources :games, only: [:index, :new, :create]
+  resources :games, only: [:index, :show, :new, :create]
 end
