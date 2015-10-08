360a7638ca8354ff769ed687b71546640cf1d8aa
Ian Campbell <ianwcampbell.contact@gmail.com>
2015-10-08 11:19:50 -0400

    hound

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,122 @@
 #stage population
-Stage.create(name: "Final Destination", legality: "Neutral", icon: "/stage_icons/final_destination_icon.jpg")
-Stage.create(name: "Fountain of Dreams", legality: "Neutral", icon: "/stage_icons/fountain_of_dreams_icon.jpg")
-Stage.create(name: "Battlefield", legality: "Neutral", icon: "/stage_icons/battlefield_icon.jpg")
-Stage.create(name: "Dream Land (N64)", legality: "Neutral", icon: "/stage_icons/dreamland_icon.jpg")
-Stage.create(name: "Yoshi's Story", legality: "Neutral", icon: "/stage_icons/yoshis_story_icon.jpg")
+Stage.create(name: "Final Destination",
+             legality: "Neutral",
+             icon: "/stage_icons/final_destination_icon.jpg"
+             )
+Stage.create(name: "Fountain of Dreams",
+             legality: "Neutral",
+             icon: "/stage_icons/fountain_of_dreams_icon.jpg"
+             )
+Stage.create(name: "Battlefield",
+             legality: "Neutral",
+             icon: "/stage_icons/battlefield_icon.jpg"
+             )
+Stage.create(name: "Dream Land (N64)",
+             legality: "Neutral",
+             icon: "/stage_icons/dreamland_icon.jpg"
+             )
+Stage.create(name: "Yoshi's Story",
+             legality: "Neutral",
+             icon: "/stage_icons/yoshis_story_icon.jpg"
+             )
 
-Stage.create(name: "Pokémon Stadium", legality: "Counterpick", icon: "/stage_icons/pokemon_stadium_icon.jpg")
+Stage.create(name: "Pokémon Stadium",
+             legality: "Counterpick",
+             icon: "/stage_icons/pokemon_stadium_icon.jpg"
+             )
 
-Stage.create(name: "Big Blue", legality: "Gentleman", icon: "/stage_icons/big_blue_icon.jpg")
-Stage.create(name: "Brinstar", legality: "Gentleman", icon: "/stage_icons/brinstar_icon.jpg")
-Stage.create(name: "Brinstar Depths", legality: "Gentleman", icon: "/stage_icons/brinstar_depths_icon.jpg")
-Stage.create(name: "Corneria", legality: "Gentleman", icon: "/stage_icons/corneria_icon.jpg")
-Stage.create(name: "Flat Zone", legality: "Gentleman", icon: "/stage_icons/flat_zone_icon.jpg")
-Stage.create(name: "Fourside", legality: "Gentleman", icon: "/stage_icons/fourside_icon.jpg")
-Stage.create(name: "Great Bay", legality: "Gentleman", icon: "/stage_icons/great_bay_icon.jpg")
-Stage.create(name: "Green Greens", legality: "Gentleman", icon: "/stage_icons/green_greens_icon.jpg")
-Stage.create(name: "Icicle Mountain", legality: "Gentleman", icon: "/stage_icons/icicle_mountain_icon.jpg")
-Stage.create(name: "Jungle Japes", legality: "Gentleman", icon: "/stage_icons/jungle_japes_icon.jpg")
-Stage.create(name: "Kongo Jungle", legality: "Gentleman", icon: "/stage_icons/kongo_jungle_icon.jpg")
-Stage.create(name: "Kongo Jungle (N64)", legality: "Gentleman", icon: "/stage_icons/kongo_jungle_n64_icon.jpg")
-Stage.create(name: "Mushroom Kingdom", legality: "Gentleman", icon: "/stage_icons/mushroom_kingdom_icon.jpg")
-Stage.create(name: "Mushroom Kingdom II", legality: "Gentleman", icon: "/stage_icons/mushroom_kingdom_ii_icon.jpg")
-Stage.create(name: "Mute City", legality: "Gentleman", icon: "/stage_icons/mute_city_icon.jpg")
-Stage.create(name: "Onett", legality: "Gentleman", icon: "/stage_icons/onett_icon.jpg")
-Stage.create(name: "Poké Floats", legality: "Gentleman", icon: "/stage_icons/poke_floats_icon.jpg")
-Stage.create(name: "Princess Peach's Castle", legality: "Gentleman", icon: "/stage_icons/peachs_castle_icon.jpg")
-Stage.create(name: "Rainbow Cruise", legality: "Gentleman", icon: "/stage_icons/rainbow_cruise_icon.jpg")
-Stage.create(name: "Temple", legality: "Gentleman", icon: "/stage_icons/temple_icon.jpg")
-Stage.create(name: "Venom", legality: "Gentleman", icon: "/stage_icons/venom_icon.jpg")
-Stage.create(name: "Yoshi's Island (pipes)", legality: "Gentleman", icon: "/stage_icons/yoshis_island_icon.jpg")
-Stage.create(name: "Yoshi's Island (N64)", legality: "Gentleman", icon: "/stage_icons/yoshis_island_n64_icon.jpg")
+Stage.create(name: "Big Blue",
+             legality: "Gentleman",
+             icon: "/stage_icons/big_blue_icon.jpg"
+             )
+Stage.create(name: "Brinstar",
+             legality: "Gentleman",
+             icon: "/stage_icons/brinstar_icon.jpg"
+             )
+Stage.create(name: "Brinstar Depths",
+             legality: "Gentleman",
+             icon: "/stage_icons/brinstar_depths_icon.jpg"
+             )
+Stage.create(name: "Corneria",
+             legality: "Gentleman",
+             icon: "/stage_icons/corneria_icon.jpg"
+             )
+Stage.create(name: "Flat Zone",
+             legality: "Gentleman",
+             icon: "/stage_icons/flat_zone_icon.jpg"
+             )
+Stage.create(name: "Fourside",
+             legality: "Gentleman",
+             icon: "/stage_icons/fourside_icon.jpg"
+             )
+Stage.create(name: "Great Bay",
+             legality: "Gentleman",
+             icon: "/stage_icons/great_bay_icon.jpg"
+             )
+Stage.create(name: "Green Greens",
+             legality: "Gentleman",
+             icon: "/stage_icons/green_greens_icon.jpg"
+             )
+Stage.create(name: "Icicle Mountain",
+             legality: "Gentleman",
+             icon: "/stage_icons/icicle_mountain_icon.jpg"
+             )
+Stage.create(name: "Jungle Japes",
+             legality: "Gentleman",
+             icon: "/stage_icons/jungle_japes_icon.jpg"
+             )
+Stage.create(name: "Kongo Jungle",
+             legality: "Gentleman",
+             icon: "/stage_icons/kongo_jungle_icon.jpg"
+             )
+Stage.create(name: "Kongo Jungle (N64)",
+             legality: "Gentleman",
+             icon: "/stage_icons/kongo_jungle_n64_icon.jpg"
+             )
+Stage.create(name: "Mushroom Kingdom",
+             legality: "Gentleman",
+             icon: "/stage_icons/mushroom_kingdom_icon.jpg"
+             )
+Stage.create(name: "Mushroom Kingdom II",
+             legality: "Gentleman",
+             icon: "/stage_icons/mushroom_kingdom_ii_icon.jpg"
+             )
+Stage.create(name: "Mute City",
+             legality: "Gentleman",
+             icon: "/stage_icons/mute_city_icon.jpg"
+             )
+Stage.create(name: "Onett",
+             legality: "Gentleman",
+             icon: "/stage_icons/onett_icon.jpg"
+             )
+Stage.create(name: "Poké Floats",
+             legality: "Gentleman",
+             icon: "/stage_icons/poke_floats_icon.jpg"
+             )
+Stage.create(name: "Princess Peach's Castle",
+             legality: "Gentleman",
+             icon: "/stage_icons/peachs_castle_icon.jpg"
+             )
+Stage.create(name: "Rainbow Cruise",
+             legality: "Gentleman",
+             icon: "/stage_icons/rainbow_cruise_icon.jpg"
+             )
+Stage.create(name: "Temple",
+             legality: "Gentleman",
+             icon: "/stage_icons/temple_icon.jpg"
+             )
+Stage.create(name: "Venom",
+             legality: "Gentleman",
+             icon: "/stage_icons/venom_icon.jpg"
+             )
+Stage.create(name: "Yoshi's Island (pipes)",
+             legality: "Gentleman",
+             icon: "/stage_icons/yoshis_island_icon.jpg"
+             )
+Stage.create(name: "Yoshi's Island (N64)",
+             legality: "Gentleman",
+             icon: "/stage_icons/yoshis_island_n64_icon.jpg"
+             )
 
 Character.create(name: "Dr. Mario", icon: "/ssbm_icons/dr_mario_icon.jpg")
 Character.create(name: "Mario", icon: "/ssbm_icons/mario_icon.jpg")
@@ -38,12 +125,16 @@ Character.create(name: "Bowser", icon: "/ssbm_icons/bowser_icon.jpg")
 Character.create(name: "Peach", icon: "/ssbm_icons/peach_icon.jpg")
 Character.create(name: "Yoshi", icon: "/ssbm_icons/yoshi_icon.jpg")
 Character.create(name: "Donkey Kong", icon: "/ssbm_icons/donkey_kong_icon.jpg")
-Character.create(name: "Captain Falcon", icon: "/ssbm_icons/captain_falcon_icon.jpg")
+Character.create(name: "Captain Falcon",
+                 icon: "/ssbm_icons/captain_falcon_icon.jpg"
+                 )
 Character.create(name: "Ganondorf", icon: "/ssbm_icons/ganondorf_icon.jpg")
 Character.create(name: "Falco", icon: "/ssbm_icons/falco_icon.jpg")
 Character.create(name: "Fox", icon: "/ssbm_icons/fox_icon.jpg")
 Character.create(name: "Ness", icon: "/ssbm_icons/ness_icon.jpg")
-Character.create(name: "Ice Climbers", icon: "/ssbm_icons/ice_climbers_icon.jpg")
+Character.create(name: "Ice Climbers",
+                 icon: "/ssbm_icons/ice_climbers_icon.jpg"
+                 )
 Character.create(name: "Kirby", icon: "/ssbm_icons/kirby_icon.jpg")
 Character.create(name: "Samus", icon: "/ssbm_icons/samus_icon.jpg")
 Character.create(name: "Zelda", icon: "/ssbm_icons/zelda_icon.jpg")
@@ -54,6 +145,8 @@ Character.create(name: "Pichu", icon: "/ssbm_icons/pichu_icon.jpg")
 Character.create(name: "Pikachu", icon: "/ssbm_icons/pikachu_icon.jpg")
 Character.create(name: "Jigglypuff", icon: "/ssbm_icons/jigglypuff_icon.jpg")
 Character.create(name: "Mewtwo", icon: "/ssbm_icons/mewtwo_icon.jpg")
-Character.create(name: "Mr. Game & Watch", icon: "/ssbm_icons/game_and_watch_icon.jpg")
+Character.create(name: "Mr. Game & Watch",
+                 icon: "/ssbm_icons/game_and_watch_icon.jpg"
+                 )
 Character.create(name: "Marth", icon: "/ssbm_icons/marth_icon.jpg")
 Character.create(name: "Roy", icon: "/ssbm_icons/roy_icon.jpg")
