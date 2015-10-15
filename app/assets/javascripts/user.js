d279b91050391db7487ff8a9e605353a4e249632
Ian Campbell <ianwcampbell.contact@gmail.com>
2015-10-15 11:42:01 -0400

    highcharts now shows wins and losses by user

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -7,30 +7,66 @@ $(function () {
   })
 
   .done(function(data){
-    var rivals = data[0];
-    var wins = data[1];
-    var mostPlayedNames = data[2];
-    var winsWith = data[3];
-    $("#rivals").highcharts({
-        chart: {
-            type: "bar"
-        },
-        title: {
-            text: "Top Rivals"
-        },
-        xAxis: {
-            categories: rivals
-        },
-        yAxis: {
+    var opponent_names = data[0];
+    var wins_against = data[1];
+    var losses_against = data[2];
+    var mostPlayedNames = data[3];
+    var winsWith = data[4];
+
+    $('#rivals').highcharts({
+            chart: {
+                type: 'bar'
+            },
             title: {
-                text: "Losses to each:"
-            }
-        },
-        series: [{
-            name: "Rivals",
-            data: wins
-        }]
-    });
+                text: 'Wins and Losses by Opponent'
+            },
+            xAxis: [{
+                categories: opponent_names,
+                reversed: false,
+                labels: {
+                    step: 1
+                }
+            }, { // mirror axis on right side
+                opposite: true,
+                reversed: false,
+                categories: opponent_names,
+                linkedTo: 0,
+                labels: {
+                    step: 1
+                }
+            }],
+            yAxis: {
+                title: {
+                    text: null
+                },
+                labels: {
+                    formatter: function () {
+                        return Math.abs(this.value);
+                    }
+                }
+            },
+
+            plotOptions: {
+                series: {
+                    stacking: 'normal'
+                }
+            },
+
+            tooltip: {
+                formatter: function () {
+                    return '<b>' + this.series.name + ' vs. ' + this.point.category +
+                    ': ' + Highcharts.numberFormat(Math.abs(this.point.y), 0) +'</b><br/>';
+                }
+            },
+
+            series: [{
+                name: 'Losses',
+                data: losses_against
+            }, {
+                name: 'Wins',
+                data: wins_against
+            }]
+        });
 
     $("#charawins").highcharts({
         chart: {
@@ -45,10 +81,13 @@ $(function () {
         yAxis: {
             title: {
                 text: "Wins with each:"
+            },
+            labels: {
+                step: 1
             }
         },
         series: [{
-            name: "Characters",
+            name: "Wins with",
             data: winsWith
         }]
     });
